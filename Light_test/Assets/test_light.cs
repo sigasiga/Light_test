fc6cc540a3e9e4ef5cb0fcec14b0fbc604fab230
志賀俊佑 <syunsukeshiga@gmail.com>
2017-02-28 09:59:01 +0900

    ライト配置位置決定まで実装

--- Light_test/Assets/test_light.cs
+++ Light_test/Assets/test_light.cs
@@ -9,20 +9,38 @@ public class test_light : MonoBehaviour {
 	int MyPosition;//現在地
 	Vector3 direction;//方向
 
-	int x=-1;//x方向の取得ー＞行と対応
-	int z=0;//z方向の取得ー＞列と対応
+	int lightDir_x=-1;//x方向の取得ー＞行と対応
+	int lightDir_z=0;//z方向の取得ー＞列と対応
 
-	bool flag = true;//一回だけ実行したいため用意
+	bool isCorner = true;//一回だけ実行したいため用意
+
+	Get_masu Get_masu;
+
 	//direction = new Vector3 (x, 0, z);
 
 	// Use this for initialization
 	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void Light(){
+		//マスデータ及び方向の取得
+		Get_masu = this.GetComponent<Get_masu> ();
+		Row = Get_masu.Row;
+		Colum = Get_masu.Colum;
+		lightDir_x = Get_masu.lightDir_x;
+		lightDir_z = Get_masu.lightDir_z;
+
 		//直線に光を飛ばす場合
-		if(x==0 || z == 0){
+		if(lightDir_x==0 || lightDir_z == 0){
 			MyPosition = (Row*5) + Colum;
 			Debug.Log(MyPosition);
 			for(int i=0; i<4; i++){
-				MyPosition += (x*5)+ z;
+				MyPosition += (lightDir_x*5)+ lightDir_z;
 				Debug.Log(MyPosition);
 			}
 		}
@@ -36,85 +54,81 @@ public class test_light : MonoBehaviour {
 				//行が上まできたら
 				if (Row == 0 && i != 0) {
 					//向きを変える
-					if (flag) {
-						x = -1;
-						MyPosition += (x * 5) + z;//移動方向の取得
+					if (isCorner) {
+						lightDir_x = -1;
+						MyPosition += (lightDir_x * 5) + lightDir_z;//移動方向の取得
 						Debug.Log (MyPosition);
-						flag = false;
+						isCorner = false;
 					} else {
-						MyPosition += (x * 5) + z;
+						MyPosition += (lightDir_x * 5) + lightDir_z;
 						Debug.Log (MyPosition);
 					}
 				}
 				//列が右端になったら
 				else if (Colum == 4 && i != 0) {
-					if (flag) {
-						z = -1;
-						MyPosition += (x * 5) + z;
+					if (isCorner) {
+						lightDir_z = -1;
+						MyPosition += (lightDir_x * 5) + lightDir_z;
 						Debug.Log (MyPosition);
-						flag = false;
+						isCorner = false;
 					} else {
-						MyPosition += (x * 5) + z;
+						MyPosition += (lightDir_x * 5) + lightDir_z;
 						Debug.Log (MyPosition);
 					}
 				}
 				//列が下になったら
 				else if (Row == 4 && i != 0) {
-					if (flag) {
-						x = 1;
-						MyPosition += (x * 5) + z;
+					if (isCorner) {
+						lightDir_x = 1;
+						MyPosition += (lightDir_x * 5) + lightDir_z;
 						Debug.Log (MyPosition);
-						flag = false;
+						isCorner = false;
 					} else {
-						MyPosition += (x * 5) + z;
+						MyPosition += (lightDir_x * 5) + lightDir_z;
 						Debug.Log (MyPosition);
 					}
 				}
 				//列が左端になったら
 				else if (Colum == 0 && i != 0) {
-					if (flag) {
-						z = 1;
-						MyPosition += (x * 5) + z;
+					if (isCorner) {
+						lightDir_z = 1;
+						MyPosition += (lightDir_x * 5) + lightDir_z;
 						Debug.Log (MyPosition);
-						flag = false;
+						isCorner = false;
 					} else {
-						MyPosition += (x * 5) + z;
+						MyPosition += (lightDir_x * 5) + lightDir_z;
 						Debug.Log (MyPosition);
 					}
 				}
 				//端に行くまで
 				else {
-					MyPosition += (x * 5) + z;
+					MyPosition += (lightDir_x * 5) + lightDir_z;
 					Debug.Log (MyPosition);
 					//端についたか確認するために取得している
 					//右下に進んでいる場合
-					if (x > 0 && z > 0) {
+					if (lightDir_x > 0 && lightDir_z > 0) {
 						Row++;
 						Colum++;
 					} 
 					//左下に進んでいる場合
-					else if (x > 0 && z < 0) {
+					else if (lightDir_x > 0 && lightDir_z < 0) {
 						Row++;
 						Colum--;
 					} 
 					//右上に進んでいる場合
-					else if (x < 0 && z > 0) {
+					else if (lightDir_x < 0 && lightDir_z > 0) {
 						Row--;
 						Colum++;
 					} 
 					//左上に進んでいる場合
-					else if (x < 0 && z < 0) {
+					else if (lightDir_x < 0 && lightDir_z < 0) {
 						Row--;
 						Colum--;
 					}
 				}
 
-			}		
+			}
+			isCorner = true;//コーナーフラグの初期化
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
